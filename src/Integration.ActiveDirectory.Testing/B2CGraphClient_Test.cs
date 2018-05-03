cd48e75998035582cd734c8e9b678ce24cc71392
timothy shepard <shepbop@gmail.com>
2018-05-03 11:44:26 -0400

    Added B2CGraphClient_Test

---
+++ src/Integration.ActiveDirectory.Testing/B2CGraphClient_Test.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Integration.ActiveDirectory.Testing
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
