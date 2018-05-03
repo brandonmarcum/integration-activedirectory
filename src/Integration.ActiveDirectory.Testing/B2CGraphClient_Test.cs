31e02dfa7c6e9130dce5f8d68834265e0ddc5427
timothy shepard <shepbop@gmail.com>
2018-05-03 14:06:04 -0400

    Added one test for B2CGraphClient

--- src/Integration.ActiveDirectory.Testing/B2CGraphClient_Test.cs
+++ src/Integration.ActiveDirectory.Testing/B2CGraphClient_Test.cs
@@ -1,14 +1,37 @@
 using System;
 using Xunit;
+using Integration.ActiveDirectory.Library;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace Integration.ActiveDirectory.Testing
 {
     public class B2CGraphClient_Test
     {
-        [Fact]
-        public void Test1()
+        IConfigurationRoot Configuration { get; set; }
+        public void SetConfiguration()
         {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
 
+
+        [Fact]
+        public void GetAllUsers__Test()
+        {         
+            //arrange
+            SetConfiguration();
+            B2CGraphClient client = new B2CGraphClient(Configuration["b2c:ClientId"], 
+                                                       Configuration["b2c:ClientSecret"], 
+                                                       Configuration["b2c:Tenant"]);
+
+            //act
+            var result = client.GetAllUsers("").Result;
+
+
+            //assert
+            Assert.NotEqual(0, result.Length);  //Will return more all users
         }
     }
 }
